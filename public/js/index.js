16435dbbae6cc1b97525ecdf2012cecd45d85328
nek0meshi <nekmeshi@gmail.com>
2021-10-23 13:18:05 +0900

    - ページ内リンクのスクロール移動を実装

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,14 @@ $(function() {
   $('#hamburger-menu').on('click', function () {
     $('#hamburger-menu').removeClass('isOpen');
   });
+
+  // ref. http://kyasper.com/jquery-tips/
+  $('a[href^="#"]').click(function () {
+    console.log('click');
+    var speed = 400;
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top - 30;
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+  });
 });
